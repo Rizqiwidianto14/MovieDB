750a55f53a7c075eb89ff2d385f739597a314227
Rizqi Imam Gilang <widiantorizqi@gmail.com>
2021-02-22 16:31:47 +0700

    first commit

---
+++ MovieDB/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieDB
+//
+//  Created by Rizqi Imam Gilang Widianto on 22/02/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
